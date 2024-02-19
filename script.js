cac99a7525b64f06f3d9116606da5e327fd2c127
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-20 01:13:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	const ch = text.charAt(0);
+	if(ch==" " || ch ==null)
+	{
+		return "";
+	}
+	else{
+		return ch;
+	}
+	
 }
 
 // Do not change the code below
