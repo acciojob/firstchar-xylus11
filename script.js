0e12e4141912eae80b21482a8b54e967e096e253
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-20 01:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 function firstChar(text) {
   // your code here
-	const ch = text.charAt(0);
-	if(ch==" " || ch ==null)
+	if(text)
 	{
-		return "";
+	const ch = text.charAt(0);
+		let i=0 ;
+	if(ch!==" ")
+	{
+		i++;
+		if(i>0)
+		{
+			return text.charAt(i);
+			
+		}
 	}
 	else{
 		return ch;
 	}
 	
 }
+	else
+	{
+		return ""
+	}
+}
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
